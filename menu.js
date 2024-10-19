105be7994d2283734221ef9e2ae414dcbf2d3804
Vardem0x <zendigvlad@gmail.com>
2024-10-20 02:54:10 +0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -3,16 +3,19 @@ let burgerShow = false;
 let overlay = document.querySelector(".overlay");
 let close = document.querySelector(".close");
 let closeShow = false;
+let shadow = document.querySelector(".background__shadow");
 
 burger.addEventListener("click", () => {
   if (burgerShow == false) {
     burger.style.backgroundImage = "url('./svgs/burger.svg')";
     burgerShow = true;
     overlay.style.transform = "scale(0)";
+    shadow.classList.remove("shadow");
   } else {
     burger.style.backgroundImage = "url('./svgs/burger-menu.svg')";
     burgerShow = false;
     overlay.style.transform = "scale(1)";
+    shadow.classList.add("shadow");
   }
 });
 
@@ -21,8 +24,10 @@ close.addEventListener("click", () => {
     burgerShow = true;
     overlay.style.transform = "scale(0)";
     burger.style.backgroundImage = "url('./svgs/burger.svg')";
+    shadow.classList.remove("shadow");
   } else {
     burgerShow = false;
     overlay.style.transform = "scale(1)";
+    shadow.classList.add("shadow");
   }
 });
