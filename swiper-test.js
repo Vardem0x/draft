13388ffa17af9d50562f2a59e0d2c4893065dc75
Vardem0x <zendigvlad@gmail.com>
2024-11-12 16:37:50 +0500

    Add files via upload

--- swiper-test.js
+++ swiper-test.js
@@ -1,18 +1,46 @@
 let upSwiper = null;
 const mediaSize = window.matchMedia("(max-width: 768px)");
-function initializeSwiper() {
-  if (mediaSize.matches && !upSwiper) {
+
+function initializeUpSwiper() {
+  if (!upSwiper) {
     upSwiper = new Swiper(".swiper", {
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        sliderCardPerView: "auto",
       },
-      slidesPerView: "auto",
     });
-  } else if (!mediaSize.matches && upSwiper) {
-    upSwiper.destroy(true, true);
+  } else {
+    upSwiper.disable();
     upSwiper = null;
   }
 }
-initializeSwiper();
-window.addEventListener("resize", initializeSwiper);
+
+let downSwiper = null;
+function initializeDownSwiper() {
+  if (!downSwiper) {
+    downSwiper = new Swiper(".swiper-2", {
+      pagination: {
+        el: ".swiper-pagination-2",
+        clickable: true,
+        sliderCardPerView: "auto",
+      },
+    });
+  } else {
+    downSwiper.disable();
+    downSwiper = null;
+  }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  if (this.window.matchMedia("(max-width: 768px)").matches) {
+    initializeUpSwiper();
+    initializeDownSwiper();
+  } else if (window.matchMedia("(max-width: 1280px)").matches) {
+    initializeDownSwiper();
+    upSwiper = null;
+  } else {
+    upSwiper = null;
+    downSwiper = null;
+  }
+});
--- swiper.js
+++ swiper.js
@@ -1,20 +1,20 @@
 let swiperButton = document.querySelector(".swiper__button");
 let advantages = document.querySelectorAll(".swiper-slide");
-let showAdvantages = true;
+let showAdvantages = false;
 const mediaLaptop = window.matchMedia("(max-width: 1150px)");
 swiperButton.addEventListener("click", () => {
   if (showAdvantages == false) {
     showAdvantages = true;
     swiperButton.style.transform = "rotate(0deg)";
-    advantages[3].style.display = "none";
-    advantages[4].style.display = "none";
-    advantages[5].style.display = "none";
-  } else if (showAdvantages == true) {
-    showAdvantages = false;
-    swiperButton.style.transform = "rotate(180deg)";
     advantages[3].style.display = "block";
     advantages[4].style.display = "block";
     advantages[5].style.display = "block";
+  } else if (showAdvantages == true) {
+    showAdvantages = false;
+    swiperButton.style.transform = "rotate(180deg)";
+    advantages[3].style.display = "none";
+    advantages[4].style.display = "none";
+    advantages[5].style.display = "none";
   }
 });
 
